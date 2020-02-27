da5a23228a8428b684aacfa6c9c506fd267b34c8
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-02-26 22:01:59 -0500

    Update I011_container_with_most_water_2.py

--- 011/I011_container_with_most_water_2.py
+++ 011/I011_container_with_most_water_2.py
@@ -4,19 +4,25 @@ class Solution(object):
         :param height: List[int]
         :return: int
         """
-        left = 0
-        right = len(height) - 1
-        Max = 0
+        # left = 0
+        # right = len(height) - 1
+        # Max = 0
+        #
+        # while left != right:
+        #     if height[left] < height[right]:
+        #         area = height[left] * (right - left)
+        #         left += 1
+        #     else:
+        #         area = height[right] * (right - left)
+        #         right -= 1
+        #     Max = max(area, Max)
+        # return Max
 
-        while left != right:
-            if height[left] < height[right]:
-                area = height[left] * (right - left)
-                left += 1
-            else:
-                area = height[right] * (right - left)
-                right -= 1
-            Max = max(area, Max)
-        return Max
+        l, r, area = 0, len(height) - 1, 0
+        while l < r:
+            h = min(height[l], height[r])
+            area, l, r = max(area, h * (r - l)), l + (height[l] == h), r - (height[r] == h)
+        return area
 
 
 s = Solution()
