bf2ab5990b568d4d696c8ed5ea20dbe35d540692
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2020-04-11 20:05:26 -0400

    1

---
+++ 1_2Sum/TwoSum.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def twoSum1(self, nums, target):
+        """
+        :param nums: List[int]
+        :param target: int
+        :return: List[int]
+        """
+
+        for num in nums:
+            # Determine if (target - num_one) is in our nums list
+            # These two numbers are different
+            if target - num in nums and num is not target - num:
+                # nums.index(4) = 0, nums.index(3) = 1
+                return [nums.index(num), nums.index(target - num)]
+
+    def twoSum2(self, nums, target):
+        """
+        :param nums: List[int]
+        :param target: int
+        :return: List[int]
+        """
+        # Create a dict
+        lookup = {}
+
+        # For counter and value in nums
+        for i, num in enumerate(nums):
+
+            # If num2 = target - num1 already in the lookup dict
+            # return the index1 and index2
+            if target - num in lookup:
+                return [lookup[target - num], i]
+
+            # If not, put num with index in lookup
+            lookup[num] = i
+        # Return again
+        return []
+
+
+if __name__ == "__main__":
+    nums = [4, 3, 5, 15]
+    target = 7
+    s = Solution()
+    # print(s.twoSum1(nums, target))
+    print(s.twoSum2(nums, target))
